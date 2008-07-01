b44849a5b3951fa81e76c1cbab7ecad75e241acd
Shawn M Moore <sartak@gmail.com>
2008-06-30 22:11:42 -0500

    Initial import of NetHack::Item

---
+++ lib/NetHack/Item.pm
@@ -0,0 +1,50 @@
+#!perl
+package NetHack::Item;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+
+1;
+
+__END__
+
+=head1 NAME
+
+NetHack::Item - ???
+
+=head1 VERSION
+
+Version 0.01 released ???
+
+=head1 SYNOPSIS
+
+    use NetHack::Item;
+
+=head1 DESCRIPTION
+
+
+
+=head1 AUTHOR
+
+Shawn M Moore, C<< <sartak at gmail.com> >>
+
+=head1 BUGS
+
+No known bugs.
+
+Please report any bugs through RT: email
+C<bug-nethack-item at rt.cpan.org>, or browse
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=NetHack-Item>.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2008 Shawn M Moore.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
