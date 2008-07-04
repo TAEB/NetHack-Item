7a71979f3a1398c5d6ac64b726dea0e447f81316
Shawn M Moore <sartak@gmail.com>
2008-07-04 01:08:52 -0500

    Add a Ring spoiler

---
+++ lib/NetHack/Item/Spoiler/Ring.pm
@@ -0,0 +1,126 @@
+#!/usr/bin/env perl
+package NetHack::Item::Spoiler::Ring;
+use strict;
+use warnings;
+use base 'NetHack::Item::Spoiler';
+
+use Memoize;
+memoize 'list';
+
+my @rings = map { "$_ ring" } (
+    qw/wooden granite opal clay coral moonstone jade bronze agate topaz
+    sapphire ruby diamond pearl iron brass copper twisted steel silver gold
+    ivory emerald wire engagement shiny/, 'black onyx', 'tiger eye'
+);
+
+sub list {
+    my $rings = {
+        'ring of adornment' => {
+            price      => 100,
+            chargeable => 1,
+        },
+        'ring of hunger' => {
+            price => 100,
+        },
+        'ring of protection' => {
+            price      => 100,
+            chargeable => 1,
+        },
+        'ring of protection from shape changers' => {
+            price => 100,
+        },
+        'ring of stealth' => {
+            price => 100,
+        },
+        'ring of sustain ability' => {
+            price => 100,
+        },
+        'ring of warning' => {
+            price => 100,
+        },
+        'ring of aggravate monster' => {
+            price => 150,
+        },
+        'ring of cold resistance' => {
+            price => 150,
+        },
+        'ring of gain constitution' => {
+            price      => 150,
+            chargeable => 1,
+        },
+        'ring of gain strength' => {
+            price      => 150,
+            chargeable => 1,
+        },
+        'ring of increase accuracy' => {
+            price      => 150,
+            chargeable => 1,
+        },
+        'ring of increase damage' => {
+            price      => 150,
+            chargeable => 1,
+        },
+        'ring of invisibility' => {
+            price => 150,
+        },
+        'ring of poison resistance' => {
+            price => 150,
+        },
+        'ring of see invisible' => {
+            price => 150,
+        },
+        'ring of shock resistance' => {
+            price => 150,
+        },
+        'ring of fire resistance' => {
+            price => 200,
+        },
+        'ring of free action' => {
+            price => 200,
+        },
+        'ring of levitation' => {
+            price => 200,
+        },
+        'ring of regeneration' => {
+            price => 200,
+        },
+        'ring of searching' => {
+            price => 200,
+        },
+        'ring of slow digestion' => {
+            price => 200,
+        },
+        'ring of teleportation' => {
+            price => 200,
+        },
+        'ring of conflict' => {
+            price => 300,
+        },
+        'ring of polymorph' => {
+            price => 300,
+        },
+        'ring of polymorph control' => {
+            price => 300,
+        },
+        'ring of teleport control' => {
+            price => 300,
+        },
+    };
+
+    # tag each ring with its name, weight, appearances, etc
+    for my $name (keys %$rings) {
+        my $stats = $rings->{$name};
+        $stats->{name}        = $name;
+        $stats->{type}        = 'ring';
+        $stats->{weight}    ||= 3;
+        $stats->{appearances} = \@rings
+            unless $stats->{appearance};
+    }
+
+    return $rings;
+}
+
+sub extra_appearances { @rings }
+
+1;
+
