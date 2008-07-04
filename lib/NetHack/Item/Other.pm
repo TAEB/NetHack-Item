cf31853b338710966d1d3917d4c9e98975bd6f98
Shawn M Moore <sartak@gmail.com>
2008-07-04 04:21:03 -0500

    Add an Other subclass

---
+++ lib/NetHack/Item/Other.pm
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+package NetHack::Item::Other
+use Moose;
+extends 'NetHack::Item';
+
+use constant type => "other";
+
+has is_chained_to_you => (
+    is      => 'rw',
+    isa     => 'Bool',
+    default => 0,
+);
+
+after incorporate_stats => sub {
+    my $self  = shift;
+    my $stats = shift;
+
+    $self->is_chained_to_you($stats->{chained});
+};
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
