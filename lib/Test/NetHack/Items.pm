589ca7ec855b03091a5c6fbc0bac642e8aa6fd3b
Shawn M Moore <sartak@gmail.com>
2008-07-03 22:56:21 -0500

    Add Test::NetHack::Items, which is a specialization of ::Item

---
+++ lib/Test/NetHack/Items.pm
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+package Test::NetHack::Items;
+use strict;
+use warnings;
+use Test::NetHack::Item;
+
+sub import {
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+
+    my $class = shift;
+    my @items = @_;
+
+    plan_items(@items);
+    test_items(@items);
+}
+
+1;
+
