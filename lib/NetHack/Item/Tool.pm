69ad555b83843ba51d755fca6195e7f4f0b4bea0
Shawn M Moore <sartak@gmail.com>
2008-07-04 04:10:52 -0500

    Keep track of partly used candles, and the number of candles attached to the candelabrum

--- lib/NetHack/Item/Tool.pm
+++ lib/NetHack/Item/Tool.pm
@@ -8,6 +8,26 @@ with 'TAEB::World::Item::Role::Erodable';    # pick-axe
 with 'TAEB::World::Item::Role::Lightable';   # lamps
 with 'TAEB::World::Item::Role::Wearable';    # towel
 
+has is_partly_used => (
+    is      => 'rw',
+    isa     => 'Bool',
+    default => 0,
+);
+
+has candles_attached => (
+    is      => 'rw',
+    isa     => 'Int',
+    default => 0,
+);
+
+after incorporate_stats => sub {
+    my $self  = shift;
+    my $stats = shift;
+
+    $self->is_partly_used($stats->{used});
+    $self->candles_attached($stats->{candles});
+};
+
 use constant type => "tool";
 
 __PACKAGE__->meta->make_immutable;
