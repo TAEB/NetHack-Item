80b43dbac9bceba75a7bbdef8282dca57be245c3
Shawn M Moore <sartak@gmail.com>
2008-11-24 00:09:27 -0500

    Add a HasPool role

---
+++ lib/NetHack/ItemPool/Role/HasPool.pm
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+package NetHack::ItemPool::Role::HasPool;
+use Moose::Role;
+
+has pool => (
+    is        => 'ro',
+    isa       => 'NetHack::ItemPool',
+    predicate => 'has_pool',
+    weak_ref  => 1,
+);
+
+no Moose::Role;
+
+1;
+
