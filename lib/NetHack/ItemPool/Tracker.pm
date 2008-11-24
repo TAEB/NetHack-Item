ab5aaff0eac9654be9327320c680426a66a6aff3
Shawn M Moore <sartak@gmail.com>
2008-11-23 22:57:22 -0500

    Tracker object

--- lib/NetHack/ItemPool/Tracker.pm
+++ lib/NetHack/ItemPool/Tracker.pm
@@ -2,6 +2,30 @@
 package NetHack::ItemPool::Tracker;
 use Moose;
 
+has type => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1,
+);
+
+has subtype => (
+    is        => 'ro',
+    isa       => 'Str',
+    predicate => 'has_subtype',
+);
+
+has appearance => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1,
+);
+
+has possibilities => (
+    is       => 'ro',
+    isa      => 'ArrayRef[Str]',
+    required => 1,
+);
+
 __PACKAGE__->meta->make_immutable;
 no Moose;
 
