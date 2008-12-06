4df2a18f3061098579e93bb68afb4e42c4465e3b
Shawn M Moore <sartak@gmail.com>
2008-12-06 01:30:03 -0500

    Specify chargeable => 0 for unchargeable rings

--- lib/NetHack/Item/Spoiler/Ring.pm
+++ lib/NetHack/Item/Spoiler/Ring.pm
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env perl
 package NetHack::Item::Spoiler::Ring;
 use strict;
@@ -19,29 +20,36 @@ sub _list {
             chargeable => 1,
         },
         'ring of hunger' => {
-            price => 100,
+            price      => 100,
+            chargeable => 0,
         },
         'ring of protection' => {
             price      => 100,
             chargeable => 1,
         },
         'ring of protection from shape changers' => {
-            price => 100,
+            price      => 100,
+            chargeable => 0,
         },
         'ring of stealth' => {
-            price => 100,
+            price      => 100,
+            chargeable => 0,
         },
         'ring of sustain ability' => {
-            price => 100,
+            price      => 100,
+            chargeable => 0,
         },
         'ring of warning' => {
-            price => 100,
+            price      => 100,
+            chargeable => 0,
         },
         'ring of aggravate monster' => {
-            price => 150,
+            price      => 150,
+            chargeable => 0,
         },
         'ring of cold resistance' => {
-            price => 150,
+            price      => 150,
+            chargeable => 0,
         },
         'ring of gain constitution' => {
             price      => 150,
@@ -60,49 +68,64 @@ sub _list {
             chargeable => 1,
         },
         'ring of invisibility' => {
-            price => 150,
+            price      => 150,
+            chargeable => 0,
         },
         'ring of poison resistance' => {
-            price => 150,
+            price      => 150,
+            chargeable => 0,
         },
         'ring of see invisible' => {
-            price => 150,
+            price      => 150,
+            chargeable => 0,
         },
         'ring of shock resistance' => {
-            price => 150,
+            price      => 150,
+            chargeable => 0,
         },
         'ring of fire resistance' => {
-            price => 200,
+            price      => 200,
+            chargeable => 0,
         },
         'ring of free action' => {
-            price => 200,
+            price      => 200,
+            chargeable => 0,
         },
         'ring of levitation' => {
-            price => 200,
+            price      => 200,
+            chargeable => 0,
         },
         'ring of regeneration' => {
-            price => 200,
+            price      => 200,
+            chargeable => 0,
         },
         'ring of searching' => {
-            price => 200,
+            price      => 200,
+            chargeable => 0,
         },
         'ring of slow digestion' => {
-            price => 200,
+            price      => 200,
+            chargeable => 0,
         },
         'ring of teleportation' => {
-            price => 200,
+            price      => 200,
+            chargeable => 0,
         },
         'ring of conflict' => {
-            price => 300,
+            price      => 300,
+            chargeable => 0,
         },
         'ring of polymorph' => {
-            price => 300,
+            price      => 300,
+            chargeable => 0,
         },
         'ring of polymorph control' => {
-            price => 300,
+            price      => 300,
+            chargeable => 0,
         },
         'ring of teleport control' => {
-            price => 300,
+            price      => 300,
+            chargeable => 0,
         },
     };
 
