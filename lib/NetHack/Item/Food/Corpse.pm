c43ea39a6acaa06129c8a7f138f5d452a87fd8c4
Shawn M Moore <sartak@gmail.com>
2009-01-31 17:42:47 -0500

    Many more corpse spoiler methods

--- lib/NetHack/Item/Food/Corpse.pm
+++ lib/NetHack/Item/Food/Corpse.pm
@@ -5,7 +5,13 @@ extends 'NetHack::Item::Food';
 
 use constant subtype => 'corpse';
 
-__PACKAGE__->meta->install_spoilers(qw/monster cannibal die aggravate poisonous/);
+__PACKAGE__->meta->install_spoilers(qw/
+    acidic aggravate cannibal cold_resistance die disintegration_resistance
+    energy fire_resistance gain_level hallucination heal intelligence
+    invisibility lycanthropy monster permanent petrify poison_resistance
+    poisonous polymorph reanimates shock_resistance sleep_resistance slime
+    speed_toggle strength stun telepathy teleport_control teleportitis
+/);
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
