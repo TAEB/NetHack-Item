bc8034442b17dbf9f7bcb4756907ce329b4ad204
Shawn M Moore <sartak@gmail.com>
2009-01-31 20:05:35 -0500

    We only need to consider evolving charge if both items do the Chargeable role

--- lib/NetHack/Item/Role/Chargeable.pm
+++ lib/NetHack/Item/Role/Chargeable.pm
@@ -121,14 +121,16 @@ around is_evolution_of => sub {
     my $new = shift;
     my $old = shift;
 
-    if ($old->recharges_known && $new->recharges_known) {
-        # lost recharges?
-        return 0 if $old->recharges > $new->recharges;
+    if ($new->does(__PACKAGE__) && $old->does(__PACKAGE__)) {
+        if ($old->recharges_known && $new->recharges_known) {
+            # lost recharges?
+            return 0 if $old->recharges > $new->recharges;
 
-        # gained charges without a recharge?
-        if ($old->charges_known && $new->charges_known) {
-            return 0 if $old->recharges == $new->recharges
-                     && $old->charges < $new->charges;
+            # gained charges without a recharge?
+            if ($old->charges_known && $new->charges_known) {
+                return 0 if $old->recharges == $new->recharges
+                        && $old->charges < $new->charges;
+            }
         }
     }
 
