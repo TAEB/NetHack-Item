076dd26dfb57bbb2cadbaebf5586fb65dca65d9b
Shawn M Moore <sartak@gmail.com>
2009-02-07 02:34:01 -0500

    Load all tracker subclasses

--- lib/NetHack/ItemPool/Tracker.pm
+++ lib/NetHack/ItemPool/Tracker.pm
@@ -4,6 +4,13 @@ use Moose;
 use Set::Object;
 with 'NetHack::ItemPool::Role::HasPool';
 
+use Module::Pluggable (
+    search_path => __PACKAGE__,
+    require     => 1,
+    sub_name    => 'tracker_types',
+);
+__PACKAGE__->tracker_types; # load all
+
 has type => (
     is       => 'ro',
     isa      => 'Str',
