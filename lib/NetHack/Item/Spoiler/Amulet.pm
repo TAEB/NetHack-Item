81a6c7337830a542ac4dfde1195fca17b9e663fa
Stefan O'Rear <stefanor@cox.net>
2009-02-22 03:24:32 -0500

    amulet materials

--- lib/NetHack/Item/Spoiler/Amulet.pm
+++ lib/NetHack/Item/Spoiler/Amulet.pm
@@ -17,6 +17,7 @@ sub _list {
             artifact => 1,
             fullname => 'The Amulet of Yendor',
             appearance => 'Amulet of Yendor',
+            material => 'mithril',
         },
 
         'Eye of the Aethiopica' => {
@@ -25,37 +26,41 @@ sub _list {
             artifact => 1,
             fullname => 'The Eye of the Aethiopica',
             base     => 'amulet of ESP',
+            material => 'iron',
         },
 
         'cheap plastic imitation of the Amulet of Yendor' => {
             price      => 0,
             appearance => 'Amulet of Yendor',
+            material   => 'plastic',
         },
 
         'amulet of change' => {
-            edible => 1,
+            edible => 1, material => 'iron',
         },
         'amulet of ESP' => {
-            edible => 1,
+            edible => 1, material => 'iron',
         },
         'amulet of life saving' => {
+            material => 'iron',
         },
         'amulet of magical breathing' => {
-            edible => 1,
+            edible => 1, material => 'iron',
         },
         'amulet of reflection' => {
+            material => 'iron',
         },
         'amulet of restful sleep' => {
-            edible => 1,
+            edible => 1, material => 'iron',
         },
         'amulet of strangulation' => {
-            edible => 1,
+            edible => 1, material => 'iron',
         },
         'amulet of unchanging' => {
-            edible => 1,
+            edible => 1, material => 'iron',
         },
         'amulet versus poison' => {
-            edible => 1,
+            edible => 1, material => 'iron',
         },
     };
 
