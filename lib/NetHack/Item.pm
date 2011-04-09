e7d6be4c5bdd4c27cc89d6aa00db196147837acd
Shawn M Moore <sartak@bestpractical.com>
2011-04-09 15:39:09 -0400

    Update copyright

--- lib/NetHack/Item.pm
+++ lib/NetHack/Item.pm
@@ -812,7 +812,7 @@ L<http://sartak.org/code/TAEB/>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2009 Shawn M Moore.
+Copyright 2009-2011 Shawn M Moore.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
